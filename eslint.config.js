49c287654e1291d74900bc7d6f43ae74dc373341
Paul Hazelton <hazeltonspaul@gmail.com>
2026-05-27 17:03:57 -0400

    Changed linter and formatted indents

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+// export default defineConfig([
+// 	globalIgnores(["dist"]),
+// 	{
+// 		files: ["**/*.{ts,tsx}"],
+// 		extends: [
+// 			js.configs.recommended,
+// 			tseslint.configs.recommended,
+// 			reactHooks.configs.flat.recommended,
+// 			reactRefresh.configs.vite,
+// 		],
+// 		languageOptions: {
+// 			globals: globals.browser,
+// 		},
+// 	},
+// ]);
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-])
+	globalIgnores(["dist"]),
+	{
+		files: ["**/*.{ts,tsx}"],
+		extends: [
+			// Other configs...
+
+			// Remove tseslint.configs.recommended and replace with this
+			tseslint.configs.recommendedTypeChecked,
+			// Alternatively, use this for stricter rules
+			tseslint.configs.strictTypeChecked,
+			// Optionally, add this for stylistic rules
+			tseslint.configs.stylisticTypeChecked,
+			// Other configs...
+		],
+		languageOptions: {
+			parserOptions: {
+				project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+				tsconfigRootDir: import.meta.dirname,
+			},
+			// other options...
+		},
+	},
+]);
