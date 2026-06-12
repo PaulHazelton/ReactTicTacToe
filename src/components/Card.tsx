a9fb09a2c67535a57918040ff09ca29fc305ad1d
Paul Hazelton <hazeltonspaul@gmail.com>
2026-06-12 12:33:44 -0400

    Proper links to games

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,30 @@
 import { type ReactNode } from "react";
 import "./Card.css";
+import { Link } from "react-router-dom";
 
-export default function Card(props: { header: string, color: string, route?: string, children: ReactNode }) {
-
+export default function Card(
+	props: {
+		header: string;
+		color: string;
+		route?: string;
+		children: ReactNode;
+	},
+) {
 	const customStyle = { "--card-color": props.color } as React.CSSProperties;
 
-	return (
-		<a className="card" style={customStyle} href={props.route}>
-			<h3>{props.header}</h3>
-			<div className="card-content">{props.children}</div>
-		</a>
-	)
+	if (props.route == undefined) {
+		return (
+			<div className="card" style={customStyle}>
+				<h3>{props.header}</h3>
+				<div className="card-content">{props.children}</div>
+			</div>
+		);
+	} else {
+		return (
+			<Link className="card" style={customStyle} to={props.route}>
+				<h3>{props.header}</h3>
+				<div className="card-content">{props.children}</div>
+			</Link>
+		);
+	}
 }
