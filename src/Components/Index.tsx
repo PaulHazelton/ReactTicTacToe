698508aea8e395968ae2419f10c212ebb3fb05a6
Paul Hazelton <hazeltonspaul@gmail.com>
2026-06-12 10:09:46 -0400

    Small css

--- src/Components/Index.tsx
+++ src/Components/Index.tsx
@@ -2,6 +2,12 @@ export default function Index() {
 	return (
 		<>
 			<h1>Ultimate Tic Tac Toe</h1>
+			<div>
+				<h2>New Single Player Game</h2>
+			</div>
+			<div>
+				<h2>New Pass and Play Game</h2>
+			</div>
 		</>
 	)
 }
