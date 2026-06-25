112923948f90b57cd69cac0b9ff19ec5d9075cba
Paul Hazelton <hazeltonspaul@gmail.com>
2026-06-24 21:09:17 -0400

    Removed duplicated function

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -5,10 +5,10 @@ import {
 	type BoardState,
 	calcGameStatus,
 	cellIsPlayable,
-	type CellValue,
 	createBoard,
 	createGameState,
 	type GameState,
+	getWinner,
 } from "../../types/GameTypes";
 import { colorMap3 } from "../../constants/Colors";
 import { Link } from "react-router-dom";
@@ -141,29 +141,5 @@ export default function Game() {
 		}
 
 		return bigBoard;
-	}
-
-	function getWinner(boardState: BoardState): CellValue {
-		const WINNING_LINES = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8], // rows
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8], // columns
-			[0, 4, 8],
-			[2, 4, 6], // diagonals
-		];
-
-		for (const [a, b, c] of WINNING_LINES) {
-			const cell = boardState[a];
-			if (
-				cell !== " " && cell === boardState[b] && cell === boardState[c]
-			) {
-				return cell;
-			}
-		}
-
-		return " ";
 	}
 }
